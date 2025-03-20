75bfb994a50064e16a9edeb97e9bea3210bd0bc7
Aske Koed <avk@holion.dk>
2025-03-20 12:40:56 +0100

    cleanup

--- ParkinsonsUrMaui/src/Presentation/App/Controls/IBottomSheet.cs
+++ ParkinsonsUrMaui/src/Presentation/App/Controls/IBottomSheet.cs
@@ -1,8 +1,3 @@
-#if __ANDROID__ || __IOS__
-using BottomSheet;
-#elif NET8_0
-#endif
-
 
 namespace MauiCleanTodos.App.Controls;
 
@@ -17,11 +12,6 @@ public class BottomSheetControl : IBottomSheet
     {
         if (view is not View)
             return;
-        
-#if __ANDROID__ || __IOS__
-        App.Current.MainPage.ShowBottomSheet(view as View, animated);
-#elif NET8_0 
-#endif
     }
 }
 
