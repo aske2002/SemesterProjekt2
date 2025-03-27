6d561157aa6217aad059ee38583e86a091e39472
claragosvig <csgosvig@hotmail.com>
2025-03-27 09:58:56 +0100

    started creating watch page

--- tremorur/Views/HomePage.xaml.cs
+++ tremorur/Views/HomePage.xaml.cs
@@ -7,12 +7,25 @@ namespace tremorur.Views
         public HomePage(HomeViewModel viewModel)
         {
             InitializeComponent();
+<<<<<<< Updated upstream
             var version = typeof(MauiApp).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
             VersionLabel.Text = $".NET MAUI ver. {version?[..version.IndexOf('+')]}";
             BindingContext = viewModel;
             viewModel.Title = "Calendar";
             //this.SetBinding(Page.TitleProperty, static (EventsViewModel vm) => vm.Title);
             SetBinding(Page.TitleProperty, new Binding(nameof(HomeViewModel.Title)));
+=======
+            StartClock(); //starter uret
+
+        }
+        private async void StartClock()
+        {
+            while (true)
+            {
+                ClockLabel.Text = DateTime.Now.ToString("HH:mm"); //Opdater label med klokken. Henter den aktuelle tid i 24 timers format
+                await Task.Delay(1000); //sørger for at opdateringen sker hvert sekund
+            }
+>>>>>>> Stashed changes
         }
     }
 }
