b047aa839d655256e8acf736c3510dfbcd21048b
Aske Koed <avk@holion.dk>
2025-04-01 09:34:46 +0200

    remove windows background implementation

--- tremorur/Platforms/Windows/App.xaml.cs
+++ tremorur/Platforms/Windows/App.xaml.cs
@@ -8,25 +8,25 @@ public partial class App : MauiWinUIApplication
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        base.OnLaunched(args);
+    // protected override void OnLaunched(LaunchActivatedEventArgs args)
+    // {
+    //     base.OnLaunched(args);
 
-        var mauiWindow = Application.Current.Windows.First();
-        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(mauiWindow.Handler.PlatformView);
-        var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
-        var appWindow = AppWindow.GetFromWindowId(windowId);
+    //     var mauiWindow = Application.Current.Windows.First();
+    //     var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(mauiWindow.Handler.PlatformView);
+    //     var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+    //     var appWindow = AppWindow.GetFromWindowId(windowId);
 
-        // Set initial size if desired
-        appWindow.Resize(new SizeInt32(800, 600));
+    //     // Set initial size if desired
+    //     appWindow.Resize(new SizeInt32(800, 600));
 
-        // Lock aspect ratio to 4:3 (example)
-        const double aspectRatio = 4.0 / 3.0;
-        appWindow.Changed += (s, e) =>
-        {
-            var size = appWindow.Size;
-            var newHeight = (int)(size.Width / aspectRatio);
-            appWindow.Resize(new SizeInt32(size.Width, newHeight));
-        };
-    }
+    //     // Lock aspect ratio to 4:3 (example)
+    //     const double aspectRatio = 4.0 / 3.0;
+    //     appWindow.Changed += (s, e) =>
+    //     {
+    //         var size = appWindow.Size;
+    //         var newHeight = (int)(size.Width / aspectRatio);
+    //         appWindow.Resize(new SizeInt32(size.Width, newHeight));
+    //     };
+    // }
 }
