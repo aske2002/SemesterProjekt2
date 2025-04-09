ae8179a362c798adf0ced33d556f637859e397c8
Aske Koed <avk@holion.dk>
2025-04-09 10:02:39 +0200

    no fixed size screen

--- tremorur/Models/Window.cs
+++ tremorur/Models/Window.cs
@@ -8,10 +8,23 @@ namespace tremorur.Models
     {
         public WatchWindow(Page page) : base(page)
         {
-            this.MaximumWidth = 800;
-            this.MaximumHeight = 800;
-            this.MinimumHeight = 800;
-            this.MinimumWidth = 800;
+            SizeChanged += KeepWindowSquare;
+        }
+        private void KeepWindowSquare(object? sender, EventArgs e)
+        {
+            Debug.WriteLine($"Window size changed: {Width}x{Height}");
+            if (Width == 0 || Height == 0)
+                return;
+
+            // Keep the window square
+            if (Width > Height)
+            {
+                Width = Height;
+            }
+            else
+            {
+                Height = Width;
+            }
         }
     }
 }
