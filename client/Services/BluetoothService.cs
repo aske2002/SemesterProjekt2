1da6ff7c923fe6684bd8505bc7231139e2406bd3
Aske Koed <avk@holion.dk>
2025-04-12 15:25:13 +0200

    a

--- client/Services/BluetoothService.cs
+++ client/Services/BluetoothService.cs
@@ -20,6 +20,77 @@ namespace client.Services.Bluetooth
             _connection = new Connection(Address.System);
         }
 
+        public async Task EnableBleAdvertisingAsync()
+        {
+            await _adapter.SetPoweredAsync(true);
+            await _adapter.SetDiscoverableAsync(true);
+
+            var advManager = _connection.CreateProxy<ILEAdvertisingManager1>("org.bluez", "/org/bluez/hci0");
+
+            var advertisementPath = new ObjectPath("/my/advertisement");
+
+            var adv = new LEAdvertisement(advertisementPath, new Dictionary<string, object>
+            {
+                { "Type", "peripheral" },
+                { "ServiceUUIDs", new string[] { "12345678-1234-5678-1234-56789abcdef0" } },
+                { "LocalName", "MyBLEDevice" }
+            });
+
+            await _connection.RegisterObjectAsync(adv);
+
+            await advManager.RegisterAdvertisementAsync(advertisementPath, new Dictionary<string, object>());
+        }
+
+        public async Task RegisterSerialProfileAsync()
+        {
+            var profileManager = _connection.CreateProxy<IProfileManager1>("org.bluez", "/org/bluez");
+
+            var options = new Dictionary<string, object>
+    {
+        { "Name", "My Serial Port" },
+        { "Role", "server" },
+        { "RequireAuthentication", true },
+        { "RequireAuthorization", false },
+        { "Channel", (byte)22 }, // Make sure this doesn't conflict
+        { "Service", "00001101-0000-1000-8000-00805f9b34fb" } // SPP UUID
+    };
+
+            await profileManager.RegisterProfileAsync(
+                new ObjectPath("/my/serial/profile"),
+                "00001101-0000-1000-8000-00805f9b34fb",
+                options
+            );
+        }
+
+        public async Task RegisterGattServerAsync()
+        {
+            var gattApp = new GattApplication();
+            var gattService = new GattService("12345678-1234-5678-1234-56789abcdef0", true, new ObjectPath("/my/gatt/app/service0"));
+            var gattChar = new GattCharacteristic("12345678-1234-5678-1234-56789abcdef1", new ObjectPath("/my/gatt/app/service0/char0"), new[] { "read", "write" });
+
+            await _connection.RegisterObjectAsync(gattApp);
+            await _connection.RegisterObjectAsync(gattService);
+            await _connection.RegisterObjectAsync(gattChar);
+
+            var gattManager = _connection.CreateProxy<IGattManager1>("org.bluez", "/org/bluez/hci0");
+
+            await gattManager.RegisterApplicationAsync(
+                new ObjectPath("/my/gatt/app"),
+                new Dictionary<string, object>() // no options needed for now
+            );
+        }
+
+        public async Task RegisterAgentAsync()
+        {
+            var agent = new NoInputNoOutputAgent();
+            await _connection.RegisterObjectAsync(agent);
+
+            var agentManager = _connection.CreateProxy<IAgentManager1>("org.bluez", "/org/bluez");
+            await agentManager.RegisterAgentAsync(agent.ObjectPath, "NoInputNoOutput");
+            await agentManager.RequestDefaultAgentAsync(agent.ObjectPath);
+        }
+
+
         public async Task StartDiscoveryAsync() => await _adapter.StartDiscoveryAsync();
         public async Task StopDiscoveryAsync() => await _adapter.StopDiscoveryAsync();
 
@@ -47,7 +118,15 @@ namespace client.Services.Bluetooth
         public async Task<string> GetAdapterNameAsync() => await _adapter.GetNameAsync();
         public async Task SetAliasAsync(string name) => await _adapter.SetAliasAsync(name);
         public async Task<string> GetAliasAsync() => await _adapter.GetAliasAsync();
+        public async Task SetDiscoverableTimeoutAsync(uint timeout) => await _adapter.SetDiscoverableTimeoutAsync(timeout);
 
+        public async Task EnableIncomingConnectionsAsync()
+        {
+            await _adapter.SetPoweredAsync(true);
+            await _adapter.SetPairableAsync(true);
+            await _adapter.SetDiscoverableAsync(true);
+            await _adapter.SetDiscoverableTimeoutAsync(0); // 0 = forever
+        }
         public async ValueTask DisposeAsync()
         {
             _interfaceAddedWatcher?.Dispose();
@@ -76,11 +155,9 @@ namespace client.Services.Bluetooth
                 throw new Exception("No Bluetooth adapter found.");
 
             Console.WriteLine("Starting Bluetooth Discovery...");
-            await SetDiscoverableAsync(true);
-            await SetPoweredAsync(true);
-            await SetPairableAsync(true);
-            await SetAliasAsync("Tremor-ur");
-            await StartDiscoveryAsync();
+            await EnableIncomingConnectionsAsync();
+            await RegisterAgentAsync();
+            await RegisterSerialProfileAsync();
 
             var isDiscoverable = await _adapter.GetDiscoverableAsync();
             var isPowered = await _adapter.GetPoweredAsync();
@@ -97,6 +174,7 @@ namespace client.Services.Bluetooth
             Console.WriteLine($"Discovering: {isDiscovering}");
             Console.WriteLine($"Adapter Alias: {adapterAlias}");
             Console.WriteLine("Bluetooth Discovery started.");
+
 
             await WatchInterfacesAddedAsync(info =>
             {
