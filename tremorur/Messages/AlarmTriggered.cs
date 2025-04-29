f4f1237f860618089863e76c76cba3cdbd2a5921
CeciliaBuhl <cecilia.e.buhl@gmail.com>
2025-04-29 10:43:17 +0200

    AlarmTriggered event

---
+++ tremorur/Messages/AlarmTriggered.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tremorur.Messages
+{
+    public class AlarmTriggered
+    {
+        public Alarm Alarm { get; set; }
+        public AlarmTriggered(Alarm alarm)
+        {
+            alarm = Alarm;
+        }
+    }
+}
