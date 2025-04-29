a5cba324c47ccaf541e7655e029d566f4a2e8c1c
Aske Koed <avk@holion.dk>
2025-04-29 09:23:18 +0200

    remove unknessecary code in appshell

--- tremorur/AppShell.xaml.cs
+++ tremorur/AppShell.xaml.cs
@@ -1,4 +1,11 @@
 ﻿
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using Foundation;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+using UIKit;
+
 namespace tremorur
 {
     public partial class AppShell : Shell
@@ -6,49 +13,27 @@ namespace tremorur
         public AppShell()
         {
             InitializeComponent();
-            // Register the routes of the detail pages
             RegisterRoutes();
-            this.Appearing += AppShell_Appearing;
-        }
 
-        private void AppShell_Appearing(object? sender, EventArgs? e)
-        {
-            var window = Application.Current?.Windows.Count > 0 ? Application.Current.Windows[0] : null;
-            if (window != null)
+#if MACCATALYST
+            HandlerChanged += (s, e) =>
             {
-                window.SizeChanged += WindowSizeChanged;
-            }
+                if (Handler is ShellRenderer shellRenderer)
+                {
+                    var tabbarView = shellRenderer.ChildViewControllers.OfType<ShellItemRenderer>().FirstOrDefault()?.View;
+                    if (tabbarView != null)
+                    {
+                        tabbarView.BackgroundColor = UIColor.Clear;
+                    }
+                }
+            };
+
+#endif
+
         }
-
-
-
-        private void WindowSizeChanged(object? sender, EventArgs? e)
-        {
-            // if (sender is Window window && CurrentPage != null)
-            // {
-            //     var widthOverflow = window.Width - CurrentPage.Bounds.Width;
-            //     var heightOverflow = window.Height - CurrentPage.Bounds.Height;
-
-            //     var idealWidth = 800 + widthOverflow;
-            //     var idealHeight = 800 + heightOverflow;
-
-            //     if (window.Width != idealWidth)
-            //     {
-            //         window.MaximumWidth = idealWidth;
-            //         window.MinimumWidth = idealWidth;
-            //     }
-
-            //     if (window.Height != idealHeight)
-            //     {
-            //         window.MaximumHeight = idealHeight;
-            //         window.MinimumHeight = idealHeight;
-            //     }
-
-            // }
-        }
-
         private void RegisterRoutes()
         {
+            BackgroundColor = Transparent;
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
