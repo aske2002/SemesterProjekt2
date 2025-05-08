af570427dac30ab0ee92d738c705043f60434a78
RoukhashIssa <roukhashi1214@gmail.com>
2025-05-08 09:50:06 +0200

    fixed alarm list design

--- tremorur/Views/AlarmListPage.xaml.cs
+++ tremorur/Views/AlarmListPage.xaml.cs
@@ -1,83 +1,58 @@
 ﻿using tremorur.Models;
 using tremorur.ViewModels;
 
-namespace tremorur.Views
+namespace tremorur.Views;
+
+public partial class AlarmListPage : ContentPageWithButtons
 {
-    public partial class AlarmListPage : ContentPage
+    private readonly AlarmListViewModel _viewModel;
+    private bool isPressed = false;
+    private DateTime pressedTime;
 
+    public AlarmListPage(AlarmListViewModel viewModel, IButtonService buttonService)
+        : base(buttonService)
     {
-        private readonly AlarmListViewModel _viewModel;
-        private ListView _alarmListView;
-
-        public AlarmListPage(AlarmListViewModel viewModel)
-
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+    protected override void OnCancelButtonHeld(object? sender, int ms, Action didHandle)
+    {
+        if (ms >= 4000)
         {
-            _viewModel = viewModel;
-
-            Title = "Medicin Alarmer";
-
-            _alarmListView = new ListView
+            didHandle();
+            if (_viewModel.SelectedAlarm != null)
             {
-                ItemsSource = _viewModel.Alarms,
-                ItemTemplate = new DataTemplate(() =>
-                {
-                    var textCell = new TextCell();
-                    textCell.SetBinding(TextCell.TextProperty, nameof(Alarm.TimeSpan));
-                    return textCell;
-                }),
-                SelectionMode = ListViewSelectionMode.Single
-            };
-
-            _alarmListView.ItemSelected += (s, e) =>
-            {
-                _viewModel.SelectedAlarm = e.SelectedItem as Alarm;
-            };
-
-            Content = _alarmListView;
-            var cancelButton = new Button
-            {
-                Text = "Cancel Alarm",
-                BackgroundColor = Colors.Red,
-                TextColor = Colors.White
-            };
-
-            bool isPressed = false;
-            DateTime pressedTime = DateTime.Now;
-
-            cancelButton.Pressed += (s, e) =>
-            {
-                isPressed = true;
-                pressedTime = DateTime.Now;
-            };
-
-            cancelButton.Released += (s, e) =>
-            {
-                if (isPressed)
-                {
-                    isPressed = false;
-                    var duration = (DateTime.Now - pressedTime).TotalMilliseconds;
-                    if (duration >= 4000) // 4 seconds
-                    {
-                        _viewModel.DeleteSelectedAlarm();
-                    }
-                }
-            };
-
-            Content = new StackLayout
-            {
-                Padding = new Thickness(20),
-                Children =
-    {
-        _alarmListView,
-        cancelButton
-    }
-            };
-
-
+                _viewModel.DeleteSelectedAlarm();
+            }
         }
+    }
+    
+
+    private async void Cancel(object sender, EventArgs e)
+{
+    if (isPressed)
+    {
+        isPressed = false;
+        var duration = (DateTime.Now - pressedTime).TotalMilliseconds;
+
+        if (duration >= 4000)
+        {
+            if (_viewModel.SelectedAlarm != null)
+            {
+                _viewModel.DeleteSelectedAlarm();
+                await Application.Current.MainPage.DisplayAlert("Success", "Alarm slettet", "OK");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Info", "Ingen alarm valgt", "OK");
+            }
+        }
+    }
+}
+}
 
 
-    }
-    }
+
 
 
