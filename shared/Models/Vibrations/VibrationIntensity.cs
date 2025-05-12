ed6e7af0567fb7a94d619ccc927468e81705322f
Aske Koed <askevkoed@gmail.com>
2025-05-12 18:00:05 +0200

    round intensity to 2 decimals

--- shared/Models/Vibrations/VibrationIntensity.cs
+++ shared/Models/Vibrations/VibrationIntensity.cs
@@ -6,8 +6,7 @@ public record VibrationIntensity
 {
     public double PercentageValue;
 
-    //Lav procent om til duty cycle => omskriv 0-1 til 0-255
-    public double AsDutyCycle(double minDutyCycle = 0.0, double maxDutyCycle = 1.0) => PercentageValue * (maxDutyCycle - minDutyCycle) + minDutyCycle;
+    public double AsDutyCycle(double minDutyCycle = 0.0, double maxDutyCycle = 1.0) => Math.Round(PercentageValue * (maxDutyCycle - minDutyCycle) + minDutyCycle, 2);
     public VibrationIntensity(double percentageValue)
     {
         PercentageValue = Math.Clamp(percentageValue, 0, 1);
