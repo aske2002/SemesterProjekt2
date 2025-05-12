17f2bab164dfdf8a70b6ca315ddbf46b406b45bb
Aske Koed <askevkoed@gmail.com>
2025-05-12 18:14:07 +0200

    moved button clicked event to shared

--- tremorur/Messages/ButtonClicked.cs
+++ tremorur/Messages/ButtonClicked.cs
@@ -1,36 +1,6 @@
-namespace tremorur.Messages;
+using shared.Models;
 
-public enum WatchButton
-{
-    Up,
-    Down,
-    Ok,
-    Cancel,
-}
-
-public static class ButtonTypes
-{
-    public static List<BorderButton> CreateButtons(Services.IMessenger messenger) => Enum.GetValues(typeof(WatchButton))
-            .Cast<WatchButton>()
-            .Select(type => new BorderButton(messenger, type))
-            .ToList();
-
-    public static readonly Dictionary<WatchButton, string> Names = new()
-    {
-        { WatchButton.Up, "Up" },
-        { WatchButton.Down, "Down" },
-        { WatchButton.Ok, "Ok" },
-        { WatchButton.Cancel, "Cancel" }
-    };
-
-    public static readonly Dictionary<WatchButton, double> Positions = new()
-    {
-        { WatchButton.Ok, 315 },
-        { WatchButton.Cancel, 45 },
-        { WatchButton.Down, 135 },
-        { WatchButton.Up, 225 }
-    };
-}
+namespace shared.Messages;
 
 public record ButtonClickedMessage(WatchButton Button, DateTime Timestamp = default);
 public record ButtonReleasedMessage(WatchButton Button, DateTime Timestamp = default);
